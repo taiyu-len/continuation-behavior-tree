37cc2d1e1bee9880dcc19c49ec7dee1989233aa6
taiyu <taiyu.len@gmail.com>
2018-12-04 14:35:38 -0800

    unique_function: catch template errors earlier.

--- include/util/unique_function.hpp
+++ include/util/unique_function.hpp
@@ -13,12 +13,6 @@ class unique_function;
 template<typename R, typename...Args>
 class unique_function<R(Args...)>
 {
-	using function_t = R(Args...);
-	struct concept_t;
-	template<typename T, bool = std::is_invocable_r<R, T, Args...>::value>
-	struct model_t;
-	template<typename T>
-	using model = model_t<std::decay_t<T>>;
 public:
 	unique_function() noexcept = default;
 	unique_function(std::nullptr_t) noexcept {};
@@ -27,8 +21,7 @@ public:
 	unique_function& operator=(std::nullptr_t) noexcept { _ptr.reset(); };
 
 	template<typename T>
-	unique_function(T&& x)
-	: _ptr(std::make_unique<model<T>>(std::forward<T>(x))) {};
+	unique_function(T&& x);
 
 	R operator()(Args... args) const
 	{
@@ -38,10 +31,13 @@ public:
 
 	operator bool() const noexcept
 	{ return bool(_ptr); }
+
 	friend void swap(unique_function& x, unique_function& y)
 	{ std::swap(x._ptr, y._ptr); }
+
 	friend bool operator==(unique_function const& x, unique_function const& y)
 	{ return x._ptr == y._ptr; }
+
 	friend bool operator!=(unique_function const& x, unique_function const& y)
 	{ return !(x == y); }
 
@@ -53,18 +49,24 @@ private:
 	};
 
 	template<typename T>
-	struct model_t<T, true> : concept_t
+	struct model_t : concept_t
 	{
 		model_t(T&& x): _data(std::move(x)) {};
 		R operator()(Args... args) override { return _data(args...); }
 		T _data;
 	};
-	template<typename T>
-	struct model_t<T, false>
-	{
-		static_assert(sizeof(T)<0,
-			"unique_function constructed with invalid function object");
-	};
 	std::unique_ptr<concept_t> _ptr {};
 };
+
+template<typename R, typename... Args>
+template<typename T>
+unique_function<R(Args...)>::unique_function(T&& fn)
+{
+	constexpr bool x = std::is_invocable_r<R, T, Args...>::value;
+	static_assert(x, "unique_function constructed with invalid object");
+	if constexpr (x)
+	{
+		_ptr = std::make_unique<model_t<std::decay_t<T>>>(std::forward<T>(fn));
+	}
+}
 }
